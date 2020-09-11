073560b458818f58686f31832ed84258438cf976
qouque <leehw6269@gmail.com>
2020-09-11 20:41:28 +0900

    dd

--- webStudy02_MVN/src/main/java/kr/or/ddit/Constans.java
+++
@@ -1,6 +0,0 @@
-package kr.or.ddit;
-
-public class Constans {
-	public static final String IMAGESTREAMINGCOOKIENAME = "imageCookie";
-	public static final String LOGINMEMBERID = "memId";
-}
--- webStudy02_MVN/src/main/java/kr/or/ddit/servlet01/Operator.java
+++
@@ -1,6 +0,0 @@
-package kr.or.ddit.servlet01;
-
-@FunctionalInterface
-public interface Operator {
-	public long operate(int leftOp, int rightOp);
-}
